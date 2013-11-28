96b10f59f78c60b0d81ac113a7a6e7dbb0305163
David <idavemm@gmail.com>
2013-11-28 11:31:24 +0100

    Add project files

---
+++ src/Blog/CoreBundle/Controller/AuthorController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Blog\CoreBundle\Controller;
+
+use Blog\CoreBundle\Services\AuthorManager;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+/**
+ * Class AuthorController
+ *
+ * @Route("/{_locale}/author", requirements={"_locale"="en|es"}, defaults={"_locale"="en"})
+ */
+class AuthorController extends Controller
+{
+    /**
+     * Show posts by author
+     *
+     * @param string $slug
+     *
+     * @throws NotFoundHttpException
+     * @return array
+     *
+     * @Route("/{slug}")
+     * @Template()
+     */
+    public function showAction($slug)
+    {
+        $author = $this->getAuthorManager()->findBySlug($slug);
+        $posts = $this->getAuthorManager()->findPosts($author);
+
+        return array(
+            'author' => $author,
+            'posts'  => $posts
+        );
+    }
+
+    /**
+     * Get Author manager
+     *
+     * @return AuthorManager
+     */
+    private function getAuthorManager()
+    {
+        return $this->get('authorManager');
+    }
+}
